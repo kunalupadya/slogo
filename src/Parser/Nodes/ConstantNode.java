73852cacc4a000b953086fa639d500c8ba459dc5
dl199@duke.edu <dl199@duke.edu>
2019-02-22 12:31:37 -0500

    made tokenizer

---
+++ src/Parser/Nodes/ConstantNode.java
@@ -0,0 +1,4 @@
+package Parser.Nodes;
+
+public class ConstantNode {
+}
--- src/Parser/ParseCommand.java
+++ src/Parser/ParseCommand.java
@@ -1,17 +1,14 @@
 package Parser;
 
-import java.util.regex.*;
 
-/*
-For predefined commands, should I just hard code the command?
-And for user defined commands, how are we gonna store them? in xml format or just in a text file
-*/
+/**
+ * @author Louis Lee
+ */
+
 
 public class ParseCommand {
 
-    private HandleError = new HandleError();
 
-    public ParseCommand(String input){
 
-    }
+
 }

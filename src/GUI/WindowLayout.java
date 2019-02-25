73d1af2dae88d802d573140fa7dd929f5766074e
Januario Carreiro <jjc70@duke.edu>
2019-02-24 22:46:21 -0500

    Initial layout now in WindowLayout

--- src/GUI/WindowLayout.java
+++ src/GUI/WindowLayout.java
@@ -1,26 +1,21 @@
 package GUI;
 
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
 import Main.Controller;
 
-import static Main.Controller.Aller_Bd;
-import static Main.Controller.Aller_Lt;
-import static Main.Controller.Aller_LtIt;
-
 /**
- * TODO: text should not be hardcoded; we will need to modify Main.TextMaker and add a config file
- *
  * Class will contain the initial layout for the Window
  *
  * @author Januario Carreiro
  */
 public class WindowLayout {
-    private Group myContainer;
-    private Controller context;
+    private BorderPane myContainer;
+    private Controller context; // TODO: Will we need context?
 
     /**
+     * TODO: add buttons
      * TODO: add JavaDoc
      *
      * @param myScene
@@ -29,6 +24,20 @@ public class WindowLayout {
     public WindowLayout(Scene myScene, Controller context) {
         this.context = context;
 
-        var initLayout = new Group();
+        var borderPane = new BorderPane();
+        var rightBorderPane = new BorderPane();
+
+        rightBorderPane.setTop(new GUI.Windows.AvailableVars());
+        rightBorderPane.setBottom(new GUI.Windows.Editor());
+
+        borderPane.setLeft(new GUI.Windows.GraphicsArea());
+        borderPane.setRight(rightBorderPane);
+        borderPane.setBottom(new GUI.Windows.Console());
+
+        myContainer = borderPane;
+    }
+
+    public BorderPane getContainer() {
+        return myContainer;
     }
 }

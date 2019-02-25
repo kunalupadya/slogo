c527c779e0385fcfe296b566202cbdd6aa6e3262
dl199@duke.edu <dl199@duke.edu>
2019-02-24 23:52:19 -0500

    parser mostly implemented. Need commands to move forward

--- src/Parser/ExecuteCommand.java
+++ src/Parser/ExecuteCommand.java
@@ -1,4 +1,20 @@
 package Parser;
 
+
+import java.util.ArrayList;
+
+/**
+ * @author Louis Lee
+ */
+
 public class ExecuteCommand {
+
+    public ExecuteCommand(){
+    }
+
+    public void runCommands(ArrayList<Command> commandsList){
+        for(int a=0; a<commandsList.size(); a++){
+            commandsList[a].execute();
+        }
+    }
 }
--- src/Parser/HandleError.java
+++ src/Parser/HandleError.java
@@ -1,16 +1,26 @@
 package Parser;
 
-import GUI.console;
+public class HandleError{
 
-public class HandleError {
+    private final static String undefinedCommandErrorMessage = "This is an undefined syntax";
+    private final static String noInputErrorMessage = "No input";
+    private final static String syntaxErrorMessage = "Syntax Error";
+    private final static String space = " ";
 
-    public HandleError(String errorString){
+    public HandleError(){
 
     }
 
-    public void showmessageonConsole(String errormessage){
-        console.print(errormessage);
+    public void undefinedCommandErrors(String error){
+        console.show(error + space + undefinedCommandErrorMessage);
     }
 
+    public void noInputError(){
+        console.show(noInputErrorMessage);
+    }
+
+    public void syntaxError(String error){
+        console.show(syntaxErrorMessage)
+    }
 
 }

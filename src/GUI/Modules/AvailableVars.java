8a1283de3e18552ce243cc8d703a1d3a0b87f61a
David Liu <dwl23@duke.edu>
2019-02-26 02:13:07 -0500

    setContent for everything except Console and Editor

--- src/GUI/Modules/AvailableVars.java
+++ src/GUI/Modules/AvailableVars.java
@@ -1,11 +1,32 @@
 package GUI.Modules;
 
-import javafx.scene.layout.BorderPane;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO: need to get list of available variables from somewhere along the backend
+ */
 
 public class AvailableVars extends Module{
+    private VBox container;
+    private List<String> availableVars;
+    private ListView<String> availableVarsDisplay;
+    private ObservableList<String> availableVarsCollection;
+    private int listviewWidth = 120;
+    private int listviewHeight = 100;
 
     public AvailableVars(int width, int height) {
         super(width, height);
+        availableVars = new ArrayList<>();
+        availableVars.add("Hi");
+        availableVars.add("Bye");
+        //updateAvailableVars();
         setContent();
     }
 
@@ -19,6 +40,16 @@ public class AvailableVars extends Module{
 
     @Override
     protected void setContent() {
+        content.setContent(container);
+        if (availableVars != null) {
+            availableVarsCollection = FXCollections.<String>observableArrayList(availableVars);
+            availableVarsDisplay = new ListView<>(availableVarsCollection);
+            availableVarsDisplay.setOrientation(Orientation.VERTICAL);
+            availableVarsDisplay.setPrefSize(listviewWidth, listviewHeight);
+        }
+    }
 
+    private void updateAvailableVars() {
+        //availableVars = ___.getList
     }
 }
--- src/GUI/Modules/GraphicsArea.java
+++ src/GUI/Modules/GraphicsArea.java
@@ -1,12 +1,27 @@
 package GUI.Modules;
 
+import GraphicsBackend.Grid;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+/**
+ * TODO: Decide whether to have a global step based on a Timeline, or a local one only for the GraphicsArea
+ */
 
 public class GraphicsArea extends Module {
-    //private GraphicsBackend.Grid grid;
+    private VBox container;
+    private Grid grid;
+    private double FRAMES_PER_SECOND = 0.5;
+    private double MILLISECOND_IN_A_SECOND = 1000;
+    private double MILLISECOND_DELAY = MILLISECOND_IN_A_SECOND / FRAMES_PER_SECOND;
+    private Timeline animation;
 
     public GraphicsArea(int width, int height) {
         super(width, height);
+        startAnimation(MILLISECOND_DELAY);
         setContent();
     }
 
@@ -20,6 +35,22 @@ public class GraphicsArea extends Module {
 
     @Override
     protected void setContent() {
+        content.setContent(container);
+        grid = new Grid(moduleWidth, moduleWidth);
+        //grid.getAllObjects();
+        //How to initialize the grid here and get the initial state
+    }
 
+    private void startAnimation(double delay) {
+        var frame = new KeyFrame(Duration.millis(delay), e -> step());
+        animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
+    }
+
+    private void step() {
+        //How do you update the grid?
+        //Is it through add movement or getallobjects or something different?
     }
 }

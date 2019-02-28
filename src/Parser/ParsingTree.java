78b3d04af630e0ebae6d6362aa5d4722d19e3e72
Kunal Upadya <ku16@duke.edu>
2019-02-28 05:43:26 -0500

    removed some unnecessary commented code

--- src/Parser/ParsingTree.java
+++ src/Parser/ParsingTree.java
@@ -64,47 +64,4 @@ public class ParsingTree {
         }
         return parent;
     }
-
-    //we should make a new ExitCommand
-//    public Command makeTree(List<Command> commandsList, List<Token> TokensList, Command parent){
-//        Command pointer;
-//        int numOfParameters;
-//        try {
-//            for (int a = 0; a < commandsList.size(); a++) {
-//                pointer = commandsList.get(a);
-//                parent.addChildren(pointer);
-//                numOfParameters = pointer.getNumParameters();
-//                Token token = TokensList.get(a);
-//                if (token == Token.COMMAND) {
-//                    pointer.addChildrenList(commandsList.subList(a + 1, a + numOfParameters));
-//                    return makeTree(commandsList.subList(a + 1, commandsList.size()), TokensList.subList(a + 1, commandsList.size()), pointer);
-//                }
-//                if (token == Token.GROUP_START) {
-//                    for (int b = a; b < commandsList.size(); b++)
-//                        if (TokensList.get(b) == Token.GROUP_END) {
-//                            pointer.addChildrenList(commandsList.subList(a + 1, b));
-//                            return makeTree(commandsList.subList(a + 1, b), TokensList.subList(a + 1, b), pointer);
-//                        }
-//                }
-//                if (token == Token.LIST_START) {
-//                    for (int b = a; b < commandsList.size(); b++)
-//                        if (TokensList.get(b) == Token.LIST_END) {
-//                            pointer.addChildrenList(commandsList.subList(a + 1, b));
-//                            return makeTree(commandsList.subList(a + 1, b), TokensList.subList(a + 1, b), pointer);
-//                        }
-//                }
-//                if (token == Token.CONSTANT) {
-//                    pointer.addChildren(commandsList.get(a));
-//                }
-//                if (token == Token.VARIABLE) {
-//                    pointer.addChildren(commandsList.get(a));
-//                }
-//
-//            }
-//        }catch (Exception e){
-//
-//        }
-//    return parent;
-//    }
-
 }

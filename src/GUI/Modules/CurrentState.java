7a3019f5cbdfce1e7c8373cddebf35d347c3e29d
Januario Carreiro <jjc70@duke.edu>
2019-03-02 18:12:59 -0500

    Added 2 new modules class skeletons

---
+++ src/GUI/Modules/CurrentState.java
@@ -0,0 +1,15 @@
+package GUI.Modules;
+
+import GUI.WindowLayout;
+
+public class CurrentState extends Module {
+
+    public CurrentState(int width, int height, WindowLayout myWindowLayout) {
+        super(width, height, myWindowLayout);
+    }
+
+    @Override
+    protected void setContent() {
+
+    }
+}

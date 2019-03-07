2746f30dfcbf95723079406fbe6eb9b594402474
Kunal Upadya <ku16@duke.edu>
2019-03-07 17:40:36 -0500

    refactor executecommand

--- src/Parser/ExecuteCommand.java
+++ src/Parser/ExecuteCommand.java
@@ -49,36 +49,50 @@ public class ExecuteCommand {
             return;
         }
         if (node.getClass() == MakeVariableCommand.class){
-            traverse(node.getChildren().get(EXPRESSION_INDEX));
-            node.execute(backendController);
+            handleMakeVariableCommand(node);
         }
         if (node.getClass() == ListStartCommand.class){
-            for (Command child: node.getChildren()){
-                traverse(child);
-            }
+            handleListStartCommand(node);
             return;
         }
-        for (Command child: node.getChildren()){
-            traverse(child);
-        }
+        traverseChildren(node);
+        handleAfterGenerationOfChildren(node);
+    }
+
+    private void handleAfterGenerationOfChildren(Command node) {
         if (node.getClass() == TextCommand.class){
-            node.execute(backendController);
-            System.out.println("hi");
-            for (Command child: node.getChildren()){
-                traverse(child);
-            }
+            handleTextCommand(node);
         }
         else if (node.getNumParameters() == node.getChildren().size()){
             node.execute(backendController);
-            System.out.println(node.getReturnValue());
         }
-
         else if (node.getClass() == RootCommand.class){
             return;
         }
         else{
-            throw new SyntaxError(WRONG_NUMBER_OF_PARAMETERS);
+            throw new SyntaxError(WRONG_NUMBER_OF_PARAMETERS);//TODO replace with another exception
         }
+    }
+
+    private void handleTextCommand(Command node) {
+        node.execute(backendController);
+        traverseChildren(node);
+    }
+
+    private void traverseChildren(Command node) {
+        for (Command child : node.getChildren()) {
+            traverse(child);
+        }
+    }
+
+    private void handleListStartCommand(Command node) {
+        traverseChildren(node);
+        return;
+    }
+
+    private void handleMakeVariableCommand(Command node) {
+        traverse(node.getChildren().get(EXPRESSION_INDEX));
+        node.execute(backendController);
     }
 
     private void handleEmptyChildrenCommands(Command node) {
@@ -91,6 +105,5 @@ public class ExecuteCommand {
         else{
             throw new SyntaxError(PARAMETERS_MISSING);
         }
-        return;
     }
 }

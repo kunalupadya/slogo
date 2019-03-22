47176859ce22e2dbaa96b9761d78e2e86a63a94d
David Liu <dwl23@duke.edu>
2019-03-22 17:49:32 -0400

    JavaDoc Modules

--- src/GUI/Modules/AvailableVars.java
+++ src/GUI/Modules/AvailableVars.java
@@ -7,15 +7,32 @@ import javafx.scene.control.ListView;
 import java.util.ResourceBundle;
 
 /**
- *
  * @author David Liu
  */
 
+/**
+ * AvailableVars Class that helps create and returns a VBox with two Panes within it, one for the label and one for
+ * the actual content of the view
+ * Depends on having a viable FrontendController class to communicate with it so that the controller can talk to the
+ * backend
+ * Example: Initialize AvailableVars with width 200 and height 250, with a moduleName of "AvailableVars" and a new FrontendController
+ * object
+ */
 public class AvailableVars extends CommandsAndVars{
     private FrontendController context;
     private ResourceBundle myResourceBundles;
     private ListView<String> listDisplay;
 
+    /**
+     * Constructor of AvailableVars, sets up the two Panes that contains all the necessary content and labels
+     * Also sets up the list associated with the view that will be displayed
+     * Assumes that FrontendController class works as implemented - there is a viable FrontendController to communicate
+     * with
+     * @param width the Module width
+     * @param height the Module height
+     * @param moduleName the name of the Module
+     * @param myFrontendController FrontendController object
+     */
     public AvailableVars(int width, int height, String moduleName, FrontendController myFrontendController) {
         super(width, height, moduleName, myFrontendController);
         context = getContext();
--- src/GUI/Modules/ListModule.java
+++ src/GUI/Modules/ListModule.java
@@ -12,10 +12,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
  * @author David Liu
  */
 
+/**
+ * Abstract ListModule Class that helps create and returns a VBox with two Panes within it, one for the label and one for
+ * the actual content of the view
+ * Depends on having a viable FrontendController class to communicate with it so that the controller can talk to the
+ * backend
+ * Example: Initialize a ListModule with width 200 and height 250, with a moduleName of "CurrentState" and a new FrontendController
+ * object
+ */
 public abstract class ListModule extends Module {
     private VBox container;
     private Pane content;
@@ -24,6 +31,16 @@ public abstract class ListModule extends Module {
     private ListView<String> listDisplay;
     private ObservableList<String> listCollection;
 
+    /**
+     * Constructor of ListModule, sets up the two Panes that contains all the necessary content and labels
+     * Also sets up the list associated with the view that will be displayed
+     * Assumes that FrontendController class works as implemented - there is a viable FrontendController to communicate
+     * with
+     * @param width the Module width
+     * @param height the Module height
+     * @param moduleName the name of the Module
+     * @param myFrontendController FrontendController object
+     */
     public ListModule(int width, int height, String moduleName, FrontendController myFrontendController) {
         super(width, height, moduleName, myFrontendController);
         list = new ArrayList<>();
@@ -50,14 +67,26 @@ public abstract class ListModule extends Module {
 
     protected abstract void setPlaceholder();
 
+    /**
+     * Gets list that will be displayed
+     * @return List of strings
+     */
     public List<String> getList() {
         return list;
     }
 
+    /**
+     * Gets listview that will help show the list which will be displayed
+     * @return ListView of Strings
+     */
     public ListView<String> getListDisplay() {
         return listDisplay;
     }
 
+    /**
+     * Gets observablelist that will help show the list which will be displayed
+     * @return ObservableList of Strings
+     */
     public ObservableList<String> getListCollection() {
         return listCollection;
     }
